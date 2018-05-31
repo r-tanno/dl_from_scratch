79eaf2705697e2a11e587430f94a8a2619e6bc0e
ym3k <yama3@yama3.org>
2018-05-31 19:54:29 +0900

    add Adastand

--- common/optimizer.py
+++ common/optimizer.py
@@ -163,6 +163,42 @@ class SDprop:
             self.m[key] *= self.gamma
             self.m[key] += (1 - self.gamma) * grads[key]
             if self.bs is True:
-                params[key] -= lr_t * grads[key] / (np.sqrt(self.c[key]) + 1e-7)
+                params[key] -= lr_t * grads[key] / (np.sqrt(self.c[key]) + 1e-8)
             else:
-                params[key] -= self.lr * grads[key] / (np.sqrt(self.c[key]) + 1e-7) 
+                params[key] -= self.lr * grads[key] / (np.sqrt(self.c[key]) + 1e-8)
+                
+class Adastand:
+
+    """Adastand"""
+
+    def __init__(self, lr=0.001, beta1=0.7, beta2=0.99, epsilon=1e-7):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.iter = 0
+        self.m = None
+        self.v = None
+        
+    def update(self, params, grads):
+        if self.m is None:
+            self.m, self.v = {}, {}
+            for key, val in params.items():
+                self.m[key] = np.zeros_like(val)
+                self.v[key] = np.zeros_like(val)
+        
+        self.iter += 1
+        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)         
+        
+        for key in params.keys():
+            # m, vのインクリメント処理はAdamと逆
+            #self.v[key] = self.beta2*self.v[key] + self.beta2*(1-self.beta2)*(grads[key]-self.m[key])
+            #self.m[key] = self.beta1*self.m[key] + (1-self.beta1)*(grads[key]-self.m[key])
+            self.v[key] += (1 - self.beta2) * (self.beta2 * (grads[key] - self.m[key])**2 - self.v[key])
+            self.m[key] += (1 - self.beta1) * (grads[key] - 2 * self.m[key])
+            
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + self.epsilon)
+            
+            #unbias_m += (1 - self.beta1) * (grads[key] - self.m[key]) # correct bias
+            #unbisa_b += (1 - self.beta2) * (grads[key]*grads[key] - self.v[key]) # correct bias
+            #params[key] += self.lr * unbias_m / (np.sqrt(unbisa_b) + 1e-7)
